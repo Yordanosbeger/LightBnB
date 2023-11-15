75b29b7810f02b19200ada40db73b2c2723ec008
Yordanos gebreselasie <yordibege@gmail.com>
2023-11-15 01:34:06 -0700

    Refactor getAllProperties function.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -10,17 +10,6 @@ const pool = new Pool({
   port: 5432
 })
 
-const getAllProperties = (options, limit= 10) => {
- return pool
-    .query(
-      'SELECT * FROM properties LIMIT $1', [limit])
-    .then((result) => {
-      console.log(result.rows);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
 
 
 /// Users
@@ -119,6 +108,63 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
+const getAllProperties = function (options, limit = 10) {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    if (queryParams.length === 1){
+    queryString += `WHERE owner_id = $${queryParams.length} `;
+  } else{
+    queryString += `AND owner_id = $${queryParams.length}`;
+  }
+}
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
+     if(queryParams.length === 2){
+    queryString += `WHERE minimum_price_per_night >= $${queryParams.length - 1 }AND options.maximum_price_per_night <= $${queryParams.length}`; 
+    queryString += `AND minimum_price_per_night >= $${queryParams.length - 1 }AND options.maximum_price_per_night <= $${queryParams.length} `;
+  }
+}
+
+  
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    if(queryParams.length === 1){
+    queryString += `WHERE average_rating >= $${queryParams.length}`;
+  } else {
+    queryString += `AND average_rating >= $${queryParams.length}`;
+}
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
+};
+
 
 /**
  * Add a property to the database
